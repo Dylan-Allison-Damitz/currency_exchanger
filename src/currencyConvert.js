4675f37a0c3d7f596003ee44d4a4452c7f9082e1
Dylan-Allison-Damitz <dylandamitz@gmail.com>
2021-08-17 13:09:38 -0700

    add all previous changes from weekend

--- src/currencyConvert.js
+++ src/currencyConvert.js
@@ -1,5 +1,5 @@
 export default class ConvertCurrency {  
-  static async getCurrency(currency) {
+  static async getConversion() {
     try {
       let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
       if (!response.ok) {
--- src/index.js
+++ src/index.js
@@ -2,5 +2,34 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ConvertCurrency from './currencyConvert.js';
 
-$;
+function clearFields() {
+  $('#showConversion').text("");
+  $('.showErrors').text("");
+}
+
+function getElements(response) {
+  if (response.conversion_rates) {
+    let pullCurrency = response.conversion_rates[0];
+    $('#showConversion').text(pullCurrency);
+  } else {
+    $('.showErrors').text(`There was an error: ${response}`);
+  }
+}
+
+async function makeApiCall(convertedAmount) {
+  const response = await ConvertCurrency.getConversion(convertedAmount);
+  getElements(response);
+}
+
+$(document).ready(function () {
+  $('#convertCurrency').click(function () {
+    event.preventDefault();
+    let totalUSD = parseInt($('#amountUS').val());
+    let foreignCurrency = $('#chooseCurrency').val();
+    const convertedAmount = (totalUSD * foreignCurrency);
+    clearFields();
+    makeApiCall(convertedAmount);
+  });
+});
