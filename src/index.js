aa3086ec696f013f2934375bb4dba14c930a0723
Dylan-Allison-Damitz <dylandamitz@gmail.com>
2021-08-20 18:39:24 -0700

    add updates to index.html, index.js

--- src/index.js
+++ src/index.js
@@ -5,31 +5,29 @@ import './css/styles.css';
 import ConvertCurrency from './currencyConvert.js';
 
 function clearFields() {
-  $('#showConversion').text("");
+  $('.showConversion').text("");
   $('.showErrors').text("");
 }
 
-function getElements(response) {
-  if (response.conversion_rates) {
-    let pullCurrency = response.conversion_rates[0];
-    $('#showConversion').text(pullCurrency);
+function getElements(response, totalUSD, foreignCurrency) {
+  if (response.result === "success") {
+    $('.showConversion').text(`Your USD is worth: $${(response["conversion_rates"][foreignCurrency]*[totalUSD]).toFixed(2)}`);
   } else {
     $('.showErrors').text(`There was an error: ${response}`);
   }
 }
 
-async function makeApiCall(convertedAmount) {
-  const response = await ConvertCurrency.getConversion(convertedAmount);
-  getElements(response);
+async function makeApiCall(totalUSD, foreignCurrency) {
+  const response = await ConvertCurrency.getConversion();
+  getElements(response, totalUSD, foreignCurrency);
 }
 
 $(document).ready(function () {
   $('#convertCurrency').click(function () {
     event.preventDefault();
-    let totalUSD = parseInt($('#amountUS').val());
-    let foreignCurrency = $('#chooseCurrency').val();
-    const convertedAmount = (totalUSD * foreignCurrency);
+    let totalUSD = $('#usdTotal').val();
+    const foreignCurrency = $('#chooseCurrency').val();
     clearFields();
-    makeApiCall(convertedAmount);
+    makeApiCall(totalUSD, foreignCurrency);
   });
 });
